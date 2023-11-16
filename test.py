9aa583500dda4f0ce07d0427145db0486e45d98f
LeTangDan <letangdan27@gmail.com>
2023-11-16 15:47:57 +0700

    Nhập code gốc

---
+++ test.py
@@ -0,0 +1,22 @@
+t = 2
+while t > 0:
+    x = str(input("nhập chuỗi x: "))
+    a = len(x)
+    b = True
+
+    if a % 2 ==0:
+        for i in range(0, a//2, 1):
+            if x[i] != x[a-1-i]:
+                b=False
+                break
+    else:
+        print("không đối xứng")
+
+    if b:
+        print("Đây là chuỗi đối xứng")
+    else:
+        print("Không đối xứng")
+
+    t = int(input("Nhập số t: "))
+        
+print("Trò chơi kết thúc")
